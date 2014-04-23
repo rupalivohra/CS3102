a84a94e0b1b5f1e907eb33279e1a1627851071f9
Rupali Vohra <rupali2vohra@gmail.com>
2014-04-23 17:00:32 -0400

    created Cloud2D; uses Java2D graphic

---
+++ TheoryProject/src/Cloud2D.java
@@ -0,0 +1,14 @@
+import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.Rectangle;
+
+
+public class Cloud2D {
+
+	public static void generate() {
+		GraphicsConfiguration gc = null;
+		Frame f = new Frame(gc);
+		Rectangle bounds = gc.getBounds();
+		
+	}
+}
